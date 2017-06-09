e824fe9903abc2a945467eca00a995e7a09ecd18
Jordy van Opstal <jordy_van_opstal@live.nl>
2017-06-10 00:29:04 +0200

    Created shotgun ability script and incorporated it into chief's prefab

---
+++ Assets/_Project/Scripts/Player/Characters/Chief/ShotgunAbility.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using _Project.Scripts.General;
+using _Project.Scripts.Units.Abilities;
+
+namespace _Project.Scripts.Player.Characters.Chief
+{
+    class ShotgunAbility : Ability
+    {
+        [SerializeField] private float _offsetDistance;
+
+        public override void Do(ICharacterAimSource aimSource)
+        {
+            //Set correct rotation.
+            RotateTowardAim(aimSource);
+
+            //Adjust position.
+            Vector3 direction = MathHelper.DegreeToVector2(aimSource.AimingDegree);
+            Vector3 offset = direction * _offsetDistance;
+            transform.position += offset;
+
+            base.Do(aimSource);
+        }
+    }
+}
