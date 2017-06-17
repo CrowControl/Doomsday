76dbc4827b30cfd6f82761cfbc3f06a51d2a3972
Jordy van Opstal <jordy_van_opstal@live.nl>
2017-06-17 18:16:54 +0200

    Merlin's swrods get stuck in buildings now

--- Assets/_Project/Scripts/Units/Spawners/OnCollisionSpawner.cs
+++ Assets/_Project/Scripts/Units/Spawners/OnCollisionSpawner.cs
@@ -1,17 +1,64 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace _Project.Scripts.Units.Spawners
 {
     public class OnCollisionSpawner : Spawner
     {
+
+        [SerializeField] private SpawnParent _spawnParent = SpawnParent.None;
+        [SerializeField] private int _maxSpawnCount = 1;
+
+        private int _spawnCount;
+
+        private enum SpawnParent
+        {
+            None,
+            This,
+            Other
+        }
+
         private void OnCollisionEnter2D(Collision2D other)
         {
-            Spawn();
+            //Use point of collision as spawn position.
+            Vector3 collisionPoint = other.contacts[0].point;
+            Spawn(other.collider.transform, collisionPoint, transform.rotation);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            Spawn();
+            Spawn(other.transform, other.transform.position, transform.rotation);
+        }
+
+        private void MaybeSpawn(Transform otherTransform, Vector3 position)
+        {
+            //Don't spawn more then we want.
+            if (_spawnCount > _maxSpawnCount) return;
+
+            //Choose parent, spawn.
+            Transform parent = ChooseParent(otherTransform);
+            Spawn(parent, position, transform.rotation);
+            _spawnCount++;
+        }
+
+        /// <summary>
+        /// Chooses what we want to use as parent for he spawned object.
+        /// </summary>
+        /// <param name="other">The transform of the other collider in the collision. One of the options.</param>
+        /// <returns>The transform that we'll use.</returns>
+        private Transform ChooseParent(Transform other)
+        {
+            switch (_spawnParent)
+            {
+                case SpawnParent.This:
+                    return transform;
+
+                case SpawnParent.Other:
+                    return other;
+
+                default:
+                    return null;
+            }
         }
     }
 }
--- Assets/_Project/Scripts/Units/Spawners/Spawner.cs
+++ Assets/_Project/Scripts/Units/Spawners/Spawner.cs
@@ -8,10 +8,26 @@ namespace _Project.Scripts.Units.Spawners
         [SerializeField] private GameObject _spawnPrefab;
         [SerializeField] private bool _destroyOnSpawn;
 
-        protected void Spawn()
+        /// <summary>
+        /// Spawns the spawn prefab of this spawner.
+        /// </summary>
+        /// <param name="parent">If this isn't null, used to set the spawned objects parent.</param>
+        /// <param name="position">Position to spawn the object at.</param>
+        /// <param name="rotation">Rotation to spawn the object at.</param>
+        protected void Spawn(Transform parent, Vector3 position, Quaternion rotation)
         {
             //Spawn.
-            Instantiate(_spawnPrefab, transform.position, transform.rotation);
+            GameObject spawnedObject = Instantiate(_spawnPrefab, position, rotation);
+            
+            //Set parent if not null.
+            if (parent != null)
+            {
+                //We need an intermediate object because Unity messes with scale and rotation when trying to set a parent.
+                GameObject empty = new GameObject {name = "Spawn Container"};
+                empty.transform.SetParent(parent);
+
+                spawnedObject.transform.SetParent(empty.transform);
+            }
 
             //Destroy if wanted.
             if (_destroyOnSpawn)
